25959c438b9200bca0bb64ba70931559d288465a
Julia Jomon <juliajomon12@gmail.com>
2025-07-31 10:38:26 +0530

    init commit

---
+++ backend/src/test/java/com/backend/ContentRecomendation/ContentRecomendationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.ContentRecomendation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentRecomendationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
